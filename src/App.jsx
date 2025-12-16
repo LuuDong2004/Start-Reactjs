7554ea5a42bd46b73c38d0caa6f90bc8440110db
luudong <luuvandong2004@gmail.com>
2025-12-16 16:09:58 +0700

    array list

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,53 @@
 import { useState } from 'react'
 import './App.css'
-import Chuot from './chuot.jsx';
+import Chuot , {DisplayArray , DisplayList}  from './chuot.jsx';
 
 function App() {
 
    const [showChuot, setShowChuot] = useState(false);
+   const [showArray, setShowArray] = useState(false);
+   const [showList, setShowList] = useState(false);
+   
    let displayButton;
    let hiddenButton;
+   let displayMore;
+   let displayList;
    let chuot;
-    if(showChuot === false){
-       displayButton = <MyButton onClick={() => setShowChuot(true)} /> 
+
+    // hiện nút xem chuột
+    if(showChuot === false && showArray === false && showList === false){
+       displayButton = <MyButton onClick={() =>{ setShowChuot(true) ; setShowArray(false) ; setShowList(false)}} /> 
+
+       displayMore = <ShowArrayChuot onClick={() =>{ setShowArray(true) ; setShowChuot(false); setShowList(false) }} />
+
+       displayList = <ShowListChuot onClick={() =>{ setShowList(true) ; setShowChuot(false) ; setShowArray(false)}} />
        chuot = null;
     }
+      // hiện chuột và hiện nút ẩn chuột
     if(showChuot === true){
         chuot = <Chuot />;
         hiddenButton = <HiddenButton onClick={() => setShowChuot(false)} />
     }
+    // hiện nhiều chuột và hiện nút ẩn chuột
+    if(showArray === true){
+        // chuot = <DisplayArray />;
+        chuot = DisplayArray();
+        hiddenButton = <HiddenButton onClick={() => setShowArray(false)} />
+    }
+
+    if(showList === true){
+        chuot = DisplayList();
+        hiddenButton = <HiddenButton onClick={() => setShowList(false)} />
+    }
+           
   return (
     <div className="App">
       <h1>Tôi là Lưu Văn Đông!</h1>
       <h2>donglv</h2> 
       <p>Xin chào<br/>Bạn khỏe chứ!</p>
       {displayButton}
+      {displayMore}
+      {displayList}
       {chuot}
       <br/>
       {hiddenButton}
@@ -30,7 +56,13 @@ function App() {
 }
 
 function MyButton({ onClick }) {
-    return <button onClick={onClick}>Xem con chuột!</button>;
+    return <button onClick={onClick}>Xem 1 con chuột!</button>;
+}
+function ShowArrayChuot({ onClick }) {
+    return <button onClick={onClick}>Array chuột!</button>;
+}
+function ShowListChuot({ onClick }) {
+    return <button onClick={onClick}>List chuột!</button>;
 }
 function HiddenButton({ onClick }) {
     return <button onClick={onClick}>Ẩn con chuột!</button>;
--- src/chuot.jsx
+++ src/chuot.jsx
@@ -3,11 +3,14 @@ const conChuot = {
     name : "Việt",
     imageUrl : "https://cafebiz.cafebizcdn.vn/2020/1/16/photo-1-15791468238321124382721.jpg",
     imageSize: 90,
-}
+};
+
+const arrayChuot = ["Chuột nhắt", "Chuột đồng", "Chuột cống"]; // khai báo mảng
+ 
 function Chuot() {
   return (
     <>
-      <h1>{conChuot.name}</h1>
+      <h4>{conChuot.name}</h4>
       <img
         className="avatar"
         src={conChuot.imageUrl}
@@ -20,4 +23,43 @@ function Chuot() {
     </>
   );
 }
+
+function DisplayArray() {
+    return(
+       <div>
+        <h3>Đây là gia đình chuột (Array) :</h3>
+         {arrayChuot.map(item => (
+         <p>{item}</p>
+      ))}
+    </div>
+    );
+}
+
+function DisplayList() {
+  return(
+    <div>
+      <h3>Đây là gia đình chuột (List) :</h3>
+      {
+        arrayChuot.map((item, index) => (
+          <p key={index}>{item}</p>
+        ))
+      }
+      {addList()}
+    </div>
+  )
+}
+
+function addList(){
+  return(
+  <div>
+    <h4>Gia nhập gia đình chuột</h4>
+    <input type="text" placeholder="Nhập tên chuột" />
+    <button>Gia nhập</button>
+  </div>
+  );
+}
+
 export default Chuot;
+export { DisplayArray };
+export { DisplayList };
+
